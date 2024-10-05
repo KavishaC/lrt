85e2b693c6aec144004adfb2dfa6c9f28e4928be
Kavisha Chandraratne <kcchandraratne@gmail.com>
2024-10-06 01:49:22 +1000

    added host

--- lrt/settings.py
+++ lrt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zi2!p#h*+$ra*z0$g+k1v58)yldmj%h!7ye$m7bn^d23&$08ig
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.107.157.118']
 
 
 # Application definition
